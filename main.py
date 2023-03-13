fbd9ca1b20f5c42bfd958074cb2e490fd32e9191
Pascal Schärli <github@pascscha.ch>
2023-03-13 10:37:06 +0100

    Formatting, pep8 compliance

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 
-vgm_url = 'https://www.coop.ch/de/lebensmittel/getraenke/bier/multipacks-ab-12x50cl/anker-lager-bier-24x50cl/p/3458809'
+vgm_url = "https://www.coop.ch/de/lebensmittel/getraenke/bier/multipacks-ab-12x50cl/anker-lager-bier-24x50cl/p/3458809"
 html_text = requests.get(vgm_url).text
-soup = BeautifulSoup(html_text, 'html.parser')
+soup = BeautifulSoup(html_text, "html.parser")
 
 
 promotion = soup.find(attrs={"data-testauto": "productlistpromotion3458809"})
 
 # exit if promotion field does not exist
-if promotion == None:
+if promotion is None:
     print("no promotion")
     exit()
 
